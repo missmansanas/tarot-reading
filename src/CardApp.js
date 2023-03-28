55c73780739eb018c6cc6e9b3acb9c55d2c4e126
paolanocom <paolanocom@gmail.com>
2023-03-28 17:52:57 +0800

    Updated dynamic img src for major arcana

--- src/CardApp.js
+++ src/CardApp.js
@@ -3,7 +3,7 @@ import Card from "react-bootstrap/Card"
 const CardApp = ({ card }) => {
     return (
     <Card>
-    <Card.Img src={require(`./assets/${card.suit}${card.rank}.jpg`)}></Card.Img>
+    <Card.Img src={card.arcana === "Minor" ? require(`./assets/${card.suit}${card.rank}.jpg`) : require(`./assets/${card.rank}-${card.name.replace(" ","")}.jpg`)}></Card.Img>
     <Card.Body>
         <Card.Title className="display-6">{card.name}</Card.Title>
         <Card.Text className="lead">{card.description}</Card.Text>
